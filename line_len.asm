37d827613386b6b65c522d1f35aba52eab8a79ed
mposk98 <anythingand42@gmail.com>
2020-09-16 21:24:53 +0300

    add line_len.asm subprogram

---
+++ line_len.asm
@@ -0,0 +1,26 @@
+global line_len
+
+; [esp+12] - end of line byte
+; [esp+8] - line adress
+; eax - result
+
+section .text
+
+line_len:
+    push ebp
+    mov ebp, esp
+
+    xor eax, eax
+    mov ebx, [ebp+8]
+    mov ecx, [ebp+12]
+
+.handle_symbol:
+    cmp [ebx+eax], cl
+    je .quit
+    inc eax
+    jmp short .handle_symbol
+
+.quit:
+    pop ebp
+    ret
+
--- test.asm
+++ test.asm
@@ -1,17 +1,19 @@
 global _start
 
-extern str_to_num
-extern rand
+; extern str_to_num
+; extern rand
+extern line_len
 
 section .text
 _start:
-        push 1
-        call rand
+        push 10
+        push msg
+        call line_len
 lalala:
         mov ebx, 0
         mov eax, 1
         int 80h
 
 section .data
-msg db "1234"
+msg db "1234", 10
 str_len equ $-msg
